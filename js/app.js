429a3e4fef8e89ceba7a9729811ec397bb51b77a
francescodt <francescodtroiano@gmail.com>
2020-02-25 15:00:40 -0600

    Statisfying the course asks, also made all the js work

--- js/app.js
+++ js/app.js
@@ -1,70 +1,112 @@
 'use strict';
 console.log('This is working!');
 
-var username = prompt('What is your name?');
-console.log(userName);
-alert('Hello World, I mean ' + userName);
+// var userName = prompt('Hello! What is your name?');
+// console.log(userName);
+// alert('Hello, ' + userName + '. Welcome to Francesco\'s wonderful and in-progress site.');
 
-var lowerCaseName = userName.toLowerCase();
 
-if (lowerCaseName === 'Francesco'){
-    alert('Welcome back, Francesco');
-}   else {
-        alert('Welcome Back!')
-        
-}
+// function beginQuestionaire(){
+//     questionOne();
+//     questionTwo();
+//     questionThree();
+//     questionFour();
+//     questionFive();
 
-var answerOne = confirm('Do you want to begin your coding journey?');
+// }
 
-if(answerOne === true || answerOne !== true){
-    alert('Let us go on a Journey throughout time and space.');
-    console.log('Space time is going on.');
-} else {
-    console.log('Okay, stay grounded my friend.');
+// function questionOne() {
+    
+//     var questionOne = prompt('Do I enjoy all board games?')
+//     var answerOne = questionOne.toLowerCase();
 
-}
+//     console.log(answerOne);
+ 
+//     if (answerOne === 'yes' || answerOne === 'y') {
+//         alert('Wrong! I do not enjoy cooperative or competitive cooperative board games.')
+//     }   else if (answerOne === 'no' || answerOne === 'n') {
+//         alert('Correct! There are tiers to board-game hierarchy, and some are terrible.')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-var myBooleanValueTrue = true;
-var anotherBooleanValueFalse = false;
-var thirdBooleanValueTrue = true;
+// function questionTwo() {
+    
+//     var questionTwo = prompt('Do I have any neices or nephews?')
+//     var answerTwo = questionTwo.toLowerCase();
 
-if(myBooleanValueTrue && thirdBooleanValueTrue){
-    alert('the && will run if both conditions return true.');
-} else if(myBooleanValueTrue || anotherBooleanValueFalse){
-    alert('The || will run if one or both of the values return true.');
-    console.log('It is truthy just enough...');
-// this may not work, something with the if and else if. you can remove the first if, and convert the second to if and should run
-}
+//     console.log(answerTwo);
+ 
+//     if (answerTwo === 'yes' || answerTwo === 'y') {
+//         alert('Correct. I have two nephews, a neice and another gender undisclosed family member along the way.')
+//     }   else if (answerTwo === 'no' || answerTwo === 'n') {
+//         alert('Wrong, Chachi. Sorry, I mean ' + userName + '. I wish you were right though, I am not a fan of kids. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-if(thirdBooleanValueTrue && anotherBooleanValueFalse) {
-    console.log('This should not run');
+// function questionThree() {
+    
+//     var questionThree = prompt('Have I been out of the country at least 8 times?!')
+//     var answerThree = questionThree.toLowerCase();
 
-}
+//     console.log(answerThree);
+ 
+//     if (answerThree === 'yes' || answerThree === 'y') {
+//         alert('Correct. Just barely. Also I assume you\'re just guessing with this since the directive was so specific.')
+//     }   else if (answerThree === 'no' || answerThree === 'n') {
+//         alert('Wrong. I\'ve spent some time in Italy, Canada, Spain, and a few other countries that the time spent was much less notable. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-alert('Welcome nerds.');
+// function questionFour() {
+    
+//     var questionFour = prompt('Did I go to at least 12 concerts last year?')
+//     var answerFour = questionFour.toLowerCase();
 
-var spaceHandle = prompt('What is your space ship handle?')
-var spaceShipPart = prompt('What part are you seeking?')
+//     console.log(answerFour);
+ 
+//     if (answerFour === 'yes' || answerFour === 'y') {
+//         alert('Wrong. Thought the specificity would work this time, huh? Not quite. I only attended 11 last year.')
+//     }   else if (answerFour === 'no' || answerFour === 'n') {
+//         alert('Correct! Just barely, though. Honestly trying to name them all is difficult as I think about it. Not due to how much I liked or did not like them, but it\'s actually just super difficult to remember all of that. Notables were: Chvrches, Alt-J, Hippo-campus, Atmosphere. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-switch (spaceShipPart){
-    case '1':
-        console.log('This is space ship part one.');
-    break;
-    case '2':
-        console.log('This is space ship part two.');
-    break;
-    case '3':
-        console.log('This is space ship part three.');
-    break;
-    default: 
-    alert('We do not have the parts you are looking for.');
+// function questionFive() {
+    
+//     var questionFive = prompt('Is this my 5-year anniversary from when I was diagnosed with cancer? ')
+//     var answerFive = questionFive.toLowerCase();
 
-}
+//     console.log(answerFive);
+ 
+//     if (answerFive === 'yes' || answerFive === 'y') {
+//         alert('Word. That means correct, in case you weren\'t sure. Only thing I really liked about it was that it landed on a year that was easy to remember, 2015.')
+//     }   else if (answerFive === 'no' || answerFive === 'n') {
+//         alert('Wrong, though I wish it were different. Everything is all good now, and it has brought a lot of awareness to my life. I donate my hair now, and do a lot of volunteer work because of it. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-// var emptyArray = [];
 
-// var quizAnswers = ['b', 'd', 'a', 'a', 'c'];
+// beginQuestionaire();
 
-// var nestedArray = [ ['yes', 'y'], ['no', 'n'], ['no', 'n'] ];
 
-// var multiLineArray
+
+// var answerYes = ['yes', 'y', 'Yes', 'Y'];
+// var answerNo = ['no', 'n', 'No', 'N'];
+
+// var questionOne = prompt('So, ' + userName + ' are you learning to code?'); 
+// if (questionOne == answerYes) {
+//     alert('Fantastic! So am I.')
+// } else if (questionOne == answerNo) {
+//     alert('Begone, nerd!')
+
+// }
