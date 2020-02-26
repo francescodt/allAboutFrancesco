a4262b97795b3e04fc6ebcd722fd422cd5c134fb
francescodt <francescodtroiano@gmail.com>
2020-02-26 09:04:40 -0600

    added some more content to java

--- js/app.js
+++ js/app.js
@@ -1,108 +1,112 @@
 'use strict';
 console.log('This is working!');
 
-var userName = prompt('Hello! What is your name?');
-console.log(userName);
-alert('Hello, ' + userName + '. Welcome to Francesco\'s wonderful and in-progress site.');
+// var userName = prompt('Hello! What is your name?');
+// console.log(userName);
+// alert('Hello, ' + userName + '. Welcome to Francesco\'s wonderful and in-progress site.');
 
 
-function beginQuestionaire(){
-    questionOne();
-    questionTwo();
-    questionThree();
-    questionFour();
-    questionFive();
+// function beginQuestionaire(){
+//     questionOne();
+//     questionTwo();
+//     questionThree();
+//     questionFour();
+//     questionFive();
 
-}
+// }
 
-function questionOne() {
+// function questionOne() {
     
-    var questionOne = prompt('Do I enjoy all board games?')
-    var answerOne = questionOne.toLowerCase();
+//     var questionOne = prompt('Do I enjoy all board games?')
+//     var answerOne = questionOne.toLowerCase();
 
-    console.log(answerOne);
+//     console.log(answerOne);
  
-    if (answerOne === 'yes' || answerOne === 'y') {
-        alert('Wrong! I do not enjoy cooperative or competitive cooperative board games.')
-    }   else if (answerOne === 'no' || answerOne === 'n') {
-        alert('Correct! There are tiers to board-game hierarchy, and some are terrible.')
-    }   else {
-        alert('Looks like you did not enter a Yes or No. Try again, bud.');
-    }
-}
+//     if (answerOne === 'yes' || answerOne === 'y') {
+//         alert('Wrong! I do not enjoy cooperative or competitive cooperative board games.')
+//     }   else if (answerOne === 'no' || answerOne === 'n') {
+//         alert('Correct! There are tiers to board-game hierarchy, and some are terrible.')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-function questionTwo() {
+// function questionTwo() {
     
-    var questionTwo = prompt('Do I have any neices or nephews?')
-    var answerTwo = questionTwo.toLowerCase();
+//     var questionTwo = prompt('Do I have any neices or nephews?')
+//     var answerTwo = questionTwo.toLowerCase();
 
-    console.log(answerTwo);
+//     console.log(answerTwo);
  
-    if (answerTwo === 'yes' || answerTwo === 'y') {
-        alert('Correct. I have two nephews, a neice and another gender undisclosed family member along the way.')
-    }   else if (answerTwo === 'no' || answerTwo === 'n') {
-        alert('Wrong, Chachi. Sorry, I mean ' + userName + '. I wish you were right though, I am not a fan of kids. ')
-    }   else {
-        alert('Looks like you did not enter a Yes or No. Try again, bud.');
-    }
-}
+//     if (answerTwo === 'yes' || answerTwo === 'y') {
+//         alert('Correct. I have two nephews, a neice and another gender undisclosed family member along the way.')
+//     }   else if (answerTwo === 'no' || answerTwo === 'n') {
+//         alert('Wrong, Chachi. Sorry, I mean ' + userName + '. I wish you were right though, I am not a fan of kids. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-function questionThree() {
+// function questionThree() {
     
-    var questionThree = prompt('Have I been out of the country at least 8 times?!')
-    var answerThree = questionThree.toLowerCase();
+//     var questionThree = prompt('Have I been out of the country at least 8 times?!')
+//     var answerThree = questionThree.toLowerCase();
 
-    console.log(answerThree);
+//     console.log(answerThree);
  
-    if (answerThree === 'yes' || answerThree === 'y') {
-        alert('Correct. Just barely. Also I assume you\'re just guessing with this since the directive was so specific.')
-    }   else if (answerThree === 'no' || answerThree === 'n') {
-        alert('Wrong. I\'ve spent some time in Italy, Canada, Spain, and a few other countries that the time spent was much less notable. ')
-    }   else {
-        alert('Looks like you did not enter a Yes or No. Try again, bud.');
-    }
-}
+//     if (answerThree === 'yes' || answerThree === 'y') {
+//         alert('Correct. Just barely. Also I assume you\'re just guessing with this since the directive was so specific. I've been in a few countries though, but I wouldn't say I'm familiar with them like I am Italy or Canada.')
+//     }   else if (answerThree === 'no' || answerThree === 'n') {
+//         alert('Wrong. I\'ve spent some time in Italy, Canada, Spain, and a few other countries that the time spent was much less notable. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-function questionFour() {
+// function questionFour() {
     
-    var questionFour = prompt('Did I go to at least 12 concerts last year?')
-    var answerFour = questionFour.toLowerCase();
+//     var questionFour = prompt('Did I go to at least 12 concerts last year?')
+//     var answerFour = questionFour.toLowerCase();
 
-    console.log(answerFour);
+//     console.log(answerFour);
  
-    if (answerFour === 'yes' || answerFour === 'y') {
-        alert('Wrong. Thought the specificity would work this time, huh? Not quite. I only attended 11 last year.')
-    }   else if (answerFour === 'no' || answerFour === 'n') {
-        alert('Correct! Just barely, though. Honestly trying to name them all is difficult as I think about it. Not due to how much I liked or did not like them, but it\'s actually just super difficult to remember all of that. Notables were: Chvrches, Alt-J, Hippo-campus, Atmosphere. ')
-    }   else {
-        alert('Looks like you did not enter a Yes or No. Try again, bud.');
-    }
-}
+//     if (answerFour === 'yes' || answerFour === 'y') {
+//         alert('Wrong. Thought the specificity would work this time, huh? Not quite. I only attended 11 last year.')
+//     }   else if (answerFour === 'no' || answerFour === 'n') {
+//         alert('Correct! Just barely, though. Honestly trying to name them all is difficult as I think about it. Not due to how much I liked or did not like them, but it\'s actually just super difficult to remember all of that. Notables were: Chvrches, Alt-J, Hippo-campus, Atmosphere. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
-function questionFive() {
+// function questionFive() {
     
-    var questionFive = prompt('Is this my 5-year anniversary from when I was diagnosed with cancer? ')
-    var answerFive = questionFive.toLowerCase();
+//     var questionFive = prompt('Is this my 5-year anniversary from when I was diagnosed with cancer? ')
+//     var answerFive = questionFive.toLowerCase();
 
-    console.log(answerFive);
+//     console.log(answerFive);
  
-    if (answerFive === 'yes' || answerFive === 'y') {
-        alert('Word. That means correct, in case you weren\'t sure. Only thing I really liked about it was that it landed on a year that was easy to remember, 2015.')
-    }   else if (answerFive === 'no' || answerFive === 'n') {
-        alert('Wrong, though I wish it were different. Everything is all good now, and it has brought a lot of awareness to my life. I donate my hair now, and do a lot of volunteer work because of it. ')
-    }   else {
-        alert('Looks like you did not enter a Yes or No. Try again, bud.');
-    }
-}
+//     if (answerFive === 'yes' || answerFive === 'y') {
+//         alert('Word. That means correct, in case you weren\'t sure. Only thing I really liked about it was that it landed on a year that was easy to remember, 2015.')
+//     }   else if (answerFive === 'no' || answerFive === 'n') {
+//         alert('Wrong, though I wish it were different. Everything is all good now, and it has brought a lot of awareness to my life. I donate my hair now, and do a lot of volunteer work because of it. ')
+//     }   else {
+//         alert('Looks like you did not enter a Yes or No. Try again, bud.');
+//     }
+// }
 
 
-beginQuestionaire();
+// beginQuestionaire();
 
 
-function endingThing() {
-    alert('Thanks for taking this questionaire about ME, ' + userName + '.')
-}
-endingThing();
+// function endingThing() {
+//     alert('Thanks for taking this questionaire about ME, ' + userName + '.')
+// }
+// endingThing();
+
+
+
+
 
 
 
